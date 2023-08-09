27ec2f2e64d4130556857f9e7dc89b30935434a4
Bl4zr-Byt <139316911+Bl4zr-Byt@users.noreply.github.com>
2023-08-09 17:57:15 +0530

    Feature: play round function -> decides winner It lets you play a round decides winner

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 function getComputerChoice() {
   choices = ["rock", "paper", "scissors"] 
-  random_choice = Math.floor(Math.random() * 3 ) 
-  return choices[random_choice]
+  return choices[Math.floor(Math.random() * choices.length)]
 }
+
+function playRound(ps, cs) {
+  // ps -> player selection
+  // cs -> computer selection
+
+  if (ps === cs) {
+    return "You Tied!"
+
+  } else {
+
+    if (ps === "rock" && cs === "scissors") {
+      return "You Won! Rock beats Scissors"
+    }
+    else if (ps === "scissors" && cs === "paper"){
+      return "You Won! Scissors beats Paper"
+
+    } else if (ps === "paper" && cs === "rock") {
+      return "You Won! Paper beats Rock"
+    }
+    else {
+
+      if (cs === "rock" && ps === "scissors") {
+        return "You Lost! Rock beats Scissors"
+      } 
+      else if (cs === "paper" && ps === "rock") {
+        return "You Lost! Paper beats Rock"
+      }
+      else if (cs === "scissors" && ps === "paper") {
+        return "You Lost! Scissors beats Paper"
+      }
+    }
+  }
+}
+
+const playerSelection = prompt("Choose").toLowerCase();
+const computerSelection = getComputerChoice();
