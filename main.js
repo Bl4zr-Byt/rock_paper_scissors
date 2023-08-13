b13ced6cbd709a442f34838035916f1e156c0f92
Bl4zr-Byt <139316911+Bl4zr-Byt@users.noreply.github.com>
2023-08-13 16:16:46 +0530

    Remove: func to play 5 rounds

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)]
 }
 
-function playRound(playerSelection) {
-  compSelection = getComputerChoice()
-
+function playRound(playerSelection, compSelection) {
   if (playerSelection === compSelection) {
     return "You Tied!"
 
