3f11baf95038a43fcbc79091511d71afe448814b
Bl4zr-Byt <139316911+Bl4zr-Byt@users.noreply.github.com>
2023-08-14 18:31:45 +0530

    Feature: Add gameButtonfunctionaly function It checks for winner display score and the win message in dom

--- main.js
+++ main.js
@@ -33,3 +33,47 @@ function playRound(playerChoice, compChoice) {
     }
   }
 }
+function gameBtnFunctionality(pChoice) {
+  let compChoice = getComputerChoice()
+  let outcome = playRound(pChoice, compChoice)
+
+  playerChoiceDOM.textContent = pChoice
+  compChoiceDOM.textContent = compChoice
+
+  if (outcome === true) {
+    gameMsg.textContent = `You Won! ${pChoice} beats ${compChoice}`
+    scores[0] += 1
+    playerScore.textContent = scores[0]
+    console.log("win")
+  }
+  else if (outcome === false) {
+    gameMsg.textContent = `You Lost! ${compChoice} beats ${pChoice}`
+    scores[1] += 1
+    compScore.textContent = scores[1]
+    console.log("lose")
+  }
+  else {
+    gameMsg.textContent = "You Tied!"
+    console.log("tie")
+  }
+}
+
+
+// variables
+let scores = [0, 0]
+
+
+// elements
+const gameMsg = document.querySelector(".game-message")
+const playerScore = document.querySelector(".player-score")
+const compScore = document.querySelector(".comp-score")
+const playerChoiceDOM = document.querySelector(".player-choice")
+const compChoiceDOM = document.querySelector(".comp-choice")
+const rockBtn = document.querySelector("#rock")
+const paperBtn = document.querySelector("#paper")
+const scissorsBtn = document.querySelector("#scissors")
+
+// events
+rockBtn.addEventListener("click", () => gameBtnFunctionality("rock"))
+paperBtn.addEventListener("click", () => gameBtnFunctionality("paper"))
+scissorsBtn.addEventListener("click", () => gameBtnFunctionality("scissors"))
