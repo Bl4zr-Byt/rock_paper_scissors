e479f1e6415c45c63056fd601eaba656607e6fd2
Bl4zr-Byt <139316911+Bl4zr-Byt@users.noreply.github.com>
2023-08-15 11:56:31 +0530

    Feature/Remove: resetGame func, win msg, rem extra Removed extra lines of code and nesting , added some more elemnts in html and css and js Added functionalty to content so that they show up when needed

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  choices = ["rock", "paper", "scissors"] 
+  const choices = ["rock", "paper", "scissors"] 
   return choices[Math.floor(Math.random() * 3)]
 }
 
@@ -7,38 +7,42 @@ function getComputerChoice() {
 function playRound(playerChoice, compChoice) {
   if (playerChoice === compChoice) {
     return undefined;
+  }
+  if (playerChoice === "rock" && compChoice === "scissors") {
+    return true;
+  }
+  else if (playerChoice === "scissors" && compChoice === "paper") {
+    return true;
 
-  } else {
+  } else if (playerChoice === "paper" && compChoice === "rock") {
+    return true;
+  }
 
-    if (playerChoice === "rock" && compChoice === "scissors") {
-      return true;
-    }
-    else if (playerChoice === "scissors" && compChoice === "paper"){
-      return true;
-
-    } else if (playerChoice === "paper" && compChoice === "rock") {
-      return true;
-    }
-    else {
-
-      if (compChoice === "rock" && playerChoice === "scissors") {
-        return false;
-      } 
-      else if (compChoice === "paper" && playerChoice === "rock") {
-        return false;
-      }
-      else if (compChoice === "scissors" && playerChoice === "paper") {
-        return false;
-      }
-    }
+  if (compChoice === "rock" && playerChoice === "scissors") {
+    return false;
+  }
+  else if (compChoice === "paper" && playerChoice === "rock") {
+    return false;
+  }
+  else if (compChoice === "scissors" && playerChoice === "paper") {
+    return false;
   }
 }
 
 function gameBtnFunctionality(pChoice) {
-  if (score["player"] >= 5 || score["comp"] >= 5) {
-    return;
+  if (score["player"] >= 5 || score["computer"] >= 5) {
+    
+    let winner = Object.keys(score).find(key => {
+      return score[key] === Math.max(score["player"], score["computer"]);
+    }).toUpperCase()
+    
+    winMsg.textContent = `${winner} WON THE GAME, GG!!`
+    playAgainBtn.style.display = "inline";
+    winMsg.style.display = "block";
+    return; 
   }
-
+  
+  resetBtn.style.display = "inline";
   let compChoice = getComputerChoice()
   let outcome = playRound(pChoice, compChoice)
 
@@ -52,19 +56,33 @@ function gameBtnFunctionality(pChoice) {
   }
   else if (outcome === false) {
     gameMsg.textContent = `You Lost! ${compChoice} beats ${pChoice}`
-    score["comp"] += 1
-    compScore.textContent = score["comp"]
+    score["computer"] += 1
+    compScore.textContent = score["computer"]
   }
   else {
     gameMsg.textContent = "You Tied!"
   }
 }
 
+function resetGame() {
+  score["player"] = 0, score["computer"] = 0
+  playerScore.textContent = 0
+  compScore.textContent = 0
+  gameMsg.textContent = "Nothing to show"
+  playerChoiceDOM.textContent = "empty"
+  compChoiceDOM.textContent = "empty"
+  winMsg.style.display = "none";
+  playAgainBtn.style.display = "none";
+  resetBtn.style.display = "none";
+}
+
 // variables
-let score = { "player": 0, "comp": 0 };
+let score = { "player": 0, "computer": 0 };
 
 // elements
 const gameMsg = document.querySelector(".game-message")
+const heading = document.querySelector("#head")
+const winMsg = document.querySelector(".win-overlay")
 
 const playerScore = document.querySelector(".player-score")
 const compScore = document.querySelector(".comp-score")
@@ -72,7 +90,19 @@ const compScore = document.querySelector(".comp-score")
 const playerChoiceDOM = document.querySelector(".player-choice")
 const compChoiceDOM = document.querySelector(".comp-choice")
 
+const resetBtn = document.querySelector("#reset")
+const playAgainBtn = document.querySelector("#play-again")
+
 const choicesBtn = document.querySelectorAll(".choice")
+
+// Styles
+resetBtn.style.display = "none"; 
+playAgainBtn.style.display = "none";
+winMsg.style.display = "none";
+
+// Events
 choicesBtn.forEach((choice) => {
   choice.addEventListener("click", () => gameBtnFunctionality(choice.id))
 })
+resetBtn.addEventListener("click", resetGame)
+playAgainBtn.addEventListener("click", resetGame)
